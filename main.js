55706a237afc916bd337f023ebaada4883f6a3fe
Vaki Ahmed Khan <145430456+Sora2187@users.noreply.github.com>
2024-11-14 22:53:30 +0530

    Add user input function
    Uses prompt and returns either rock paper or scissor based on what user entered.

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ const getComChoice = () => {
 };
 
 console.log(getComChoice());
+
+// Player choice
+const getPlayerChoice = () => {
+  let choice = parseInt(
+    prompt("Choose either rock(1), papers(2) or scissors(3): ")
+  );
+  return choice === 1
+    ? R
+    : choice === 2
+    ? P
+    : choice === 3
+    ? S
+    : console.log("Please either choose 1, 2 or 3");
+};
+
+console.log(getPlayerChoice());
